e045f37886eb1e7396b39959a74258f6100379e2
Ines Ivanova <ines.iv.ivanova@gmail.com>
2021-10-04 22:09:48 +0300

    Add contect to the HTML app

--- Lecture1/templates_demo/main.py
+++ Lecture1/templates_demo/main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    context = {"name": "Flask"}
+    return render_template("index.html", **context)
 
 
 if __name__ == "__main__":
