1d45c609b250098f6930799186ad02a5327600aa
Ines Ivanova <ines.iv.ivanova@gmail.com>
2021-10-11 21:43:38 +0300

    Done demo lecture 3 auth and authorization + schemas

--- Lecture3/main.py
+++ Lecture3/main.py
@@ -1,14 +1,18 @@
 import enum
+from datetime import datetime, timedelta
+from functools import wraps
 
+import jwt
 from decouple import config
 from flask import Flask, request
+from flask_httpauth import HTTPTokenAuth
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort
 from flask_sqlalchemy import SQLAlchemy
 from password_strength import PasswordPolicy
 from sqlalchemy import func
 from marshmallow import Schema, fields, validate, ValidationError
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -20,6 +24,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@l
 db = SQLAlchemy(app)
 api = Api(app)
 migrate = Migrate(app, db)
+
+auth = HTTPTokenAuth(scheme='Bearer')
+
+
+@auth.verify_token
+def verify_token(token):
+    try:
+        user_id = User.decode_token(token)
+        return User.query.filter_by(id=user_id).first()
+    except Exception:
+        return 400
 
 
 policy = PasswordPolicy.from_names(
@@ -36,6 +51,19 @@ def validate_password(value):
         raise ValidationError(f"Not a valid password")
 
 
+def validate_schema(schema_name):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            schema = schema_name()
+            errors = schema.validate(request.get_json())
+            if errors:
+                abort(400, errors=errors)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
 class BaseUserSchema(Schema):
     email = fields.Email(required=True)
     full_name = fields.String(required=True, validate=validate.Length(min=2))
@@ -50,14 +78,52 @@ class UserSchema(BaseUserSchema):
     updated_on = fields.DateTime()
 
 
+class UserRolesEnum(enum.Enum):
+    super_admin = "super admin"
+    admin = "admin"
+    user = "user"
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     full_name = db.Column(db.String(255), nullable=False)
     phone = db.Column(db.Text)
+    role = db.Column(
+        db.Enum(UserRolesEnum),
+        default=UserRolesEnum.user,
+        nullable=False
+    )
     create_on = db.Column(db.DateTime, server_default=func.now())
     updated_on = db.Column(db.DateTime, onupdate=func.now())
+
+    def encode_token(self):
+        try:
+            payload = {
+                'exp': datetime.utcnow() + timedelta(days=2),
+                'sub': self.id
+            }
+            return jwt.encode(
+                payload,
+                key=config('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def decode_token(auth_token):
+        try:
+            key = config('SECRET_KEY')
+            payload = jwt.decode(jwt=auth_token, key=key,  algorithms=["HS256"])
+            return payload['sub']
+        except jwt.ExpiredSignatureError as ex:
+            raise ex
+        except jwt.InvalidTokenError as ex:
+            raise ex
+        except Exception as ex:
+            raise ex
 
 
 class ColorEnum(enum.Enum):
@@ -95,24 +161,56 @@ class Clothes(db.Model):
 
 
 class SignUp(Resource):
+    @validate_schema(UserSignInSchema)
     def post(self):
         data = request.get_json()
-        schema = UserSignInSchema()
-        errors = schema.validate(data)
+        data["password"] = generate_password_hash(data['password'], method='sha256')
+        user = User(**data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.encode_token()
+        return {"token": token}, 201
 
-        if not errors:
-            data["password"] = generate_password_hash(data['password'], method='sha256')
-            db.session.add(User(**data))
-            db.session.commit()
-            return 201, data
-        return 400
-        # The need of marshmellow schema
 
-    #pip install flask_jwt_extended, marshmallow, password_strength
+
+
+class SignIn(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(email=data["email"]).first()
+        if user and check_password_hash(user.password, data["password"]):
+            token = user.encode_token()
+            return  {"token": token}, 200
+        return {"messsage": "Wrong email or passwword"}, 400
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            user = auth.current_user()
+            if not user.role == permission:
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+class ClothesRouter(Resource):
+    @auth.login_required
+    @permission_required(UserRolesEnum.admin)
+    def get(self):
+        current_user = auth.current_user()
+        clothes = Clothes.query.all()
+        return {"data": clothes}, 200
+
+
 
     # enums, models migrations,  register, login
 db.create_all()
 api.add_resource(SignUp, "/register")
+api.add_resource(SignIn, "/login")
+api.add_resource(ClothesRouter, "/clothes")
 
 if __name__ == "__main__":
     db.create_all()
