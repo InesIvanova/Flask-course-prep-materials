7cebfb2d3b269b03734ff4245edea4918307f7eb
Ines Ivanova <ines.iv.ivanova@gmail.com>
2021-11-06 15:16:08 +0200

    testing black

--- Lecture4-Complaints-system/tests/test_sample.py
+++ Lecture4-Complaints-system/tests/test_sample.py
@@ -5,8 +5,11 @@ from unittest.mock import patch
 def mock_uuid():
     return "11111111"
 
+
 def generate_data():
     return {"name": "Test name", "id": uuid.uuid4()}
+
+
 @patch("uuid.uuid4", mock_uuid)
 def test_object_generation():
     data = generate_data()
